909dd6175f79a5b54c8ad24c906054932a8b2e97
rzhvsk <rzhevskiy@outlook.com>
2021-09-04 16:37:38 +0300

    1

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2565,7 +2565,8 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      210
+    #define E0_CURRENT      21
+    0
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
